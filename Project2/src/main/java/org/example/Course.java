dd9a580ee9f52158842de1f535d07c7f807b5099
Karim Khaleel <kkhaleel@sumerge.com>
2024-09-25 10:21:36 +0300

    Added Project1 in Task3

--- Project2/src/main/java/org/example/Course.java
+++ Project2/src/main/java/org/example/Course.java
@@ -1,41 +1,38 @@
 package org.example;
 
-import org.springframework.stereotype.Component;
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+import java.util.Objects;
+
+@Configuration
+@Data
 public class Course {
 
     private int id;
     private String name;
     private String description;
+    private int credits;
 
-    public Course(int id, String name, String description) {
+
+    public Course(int id, String name, String description, int credits) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.credits = credits;
     }
 
-    public int getId() {
-        return id;
+    public Course(){
+
     }
 
-    public void setId(int id) {
-        this.id = id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && credits == course.credits && Objects.equals(name, course.name) && Objects.equals(description, course.description);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- Project2/src/main/java/org/example/CourseRecommenderImpl2.java
+++ Project2/src/main/java/org/example/CourseRecommenderImpl2.java
@@ -12,8 +12,8 @@ import java.util.List;
 public class CourseRecommenderImpl2 implements CourseRecommender {
 
     public List<Course> recommendedCourses(){
-        Course c1 = new Course(1, "Chemistry", "Chem Course");
-        Course c2 = new Course(2, "Maths", "Maths Course");
+        Course c1 = new Course(1, "Chemistry", "Chem Course", 5);
+        Course c2 = new Course(2, "Maths", "Maths Course", 5);
         Course[] arr = {c1, c2};
         return Arrays.asList(arr);
     }
